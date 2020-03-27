1470b522eaeb5d2458c36582f92d5a696689b7bd
DAristova <dar.arist@gmail.com>
2020-03-27 18:04:01 +0200

    TDD Homework done

--- lantern/tdd/lonely_robot.py
+++ lantern/tdd/lonely_robot.py
@@ -5,13 +5,57 @@ class Asteroid:
 
 
 class Robot:
-    def __init__(self, x, y, asteroid):
+    def __init__(self, x, y, asteroid, direction):
         self.x = x
         self.y = y
         self.asteroid = asteroid
+        self.direction = direction
         if self.x > self.asteroid.x:
             raise MissAsteroidError()
+        if self.y > self.asteroid.y:
+            raise MissAsteroidError()
+
+    def turn_left(self):
+        turns_left = {'E': 'N', 'N': 'W', 'W': 'S', 'S': 'E'}
+        self.direction = turns_left[self.direction]
+
+    def turn_right(self):
+        turns_right = {'E': 'S', 'S': 'W', 'W': 'N', 'N': 'E'}
+        self.direction = turns_right[self.direction]
+
+    def move_forward(self, distance):
+        if self.direction == 'N' or self.direction == 'S':
+            if self.x + distance <= self.asteroid.x:
+                self.x + distance
+            else:
+                raise RobotFallError
+        if self.direction == 'W' or self.direction == 'E':
+            if self.y + distance <= self.asteroid.y:
+                self.y + distance
+            else:
+                raise RobotFallError
+
+    def move_backward(self, distance):
+        if self.direction == 'N' or self.direction == 'S':
+            if self.x - distance >= 0:
+                self.x - distance
+            else:
+                raise RobotFallError
+        if self.direction == 'W' or self.direction == 'E':
+            if self.y - distance >= 0:
+                self.y - distance
+            else:
+                raise RobotFallError
 
 
 class MissAsteroidError(Exception):
     pass
+
+
+class RobotFallError(Exception):
+    pass
+
+# if __name__ == '__main__':
+# asteroid = Asteroid(15, 15)
+# robot = Robot(5, 5, asteroid, 'N')
+# try: print(robot.move_backward(15))
--- lantern/tdd/test_lonely_robot_execution.py
+++ lantern/tdd/test_lonely_robot_execution.py
@@ -1,14 +1,16 @@
 import pytest
-from lonely_robot import Robot, Asteroid, MissAsteroidError
+from lonely_robot import Robot, Asteroid, MissAsteroidError, RobotFallError
 
 
 class TestRobotCreation:
     def test_parameters(self):
         x, y = 10, 15
-        asteroid = Asteroid(x, y)
-        robot = Robot(x, y, asteroid)
+        direction = 'E'
+        asteroid = Asteroid(x + 1, y + 1)
+        robot = Robot(x, y, asteroid, direction)
         assert robot.x == 10
         assert robot.y == 15
+        assert robot.direction == direction
         assert robot.asteroid == asteroid
 
     @pytest.mark.parametrize(
@@ -22,4 +24,105 @@ class TestRobotCreation:
     def test_check_if_robot_on_asteroid(self, asteroid_size, robot_coordinates):
         with pytest.raises(MissAsteroidError):
             asteroid = Asteroid(*asteroid_size)
-            Robot(*robot_coordinates, asteroid)
+            Robot(*robot_coordinates, asteroid, 'W')
+
+
+class TestRobotBehavior:
+
+    def setup(self):
+        self.x, self.y = 5, 5
+        self.asteroid = Asteroid(self.x + 10, self.y + 10)
+
+    @pytest.mark.parametrize(
+        "curent_direction,expected_direction",
+        (
+                ('E', 'N'),
+                ('N', 'W'),
+                ('W', 'S'),
+                ('S', 'E')
+        )
+    )
+    def test_turn_left(self, curent_direction, expected_direction):
+        robot = Robot(self.x, self.y, self.asteroid, curent_direction)
+        robot.turn_left()
+        assert robot.direction == expected_direction
+
+    @pytest.mark.parametrize(
+        "curent_direction, expected_direction",
+        (
+                ('E', 'S'),
+                ('S', 'W'),
+                ('W', 'N'),
+                ('N', 'E')
+        )
+    )
+    def test_turn_right(self, curent_direction, expected_direction):
+        robot = Robot(self.x, self.y, self.asteroid, curent_direction)
+        robot.turn_right()
+        assert robot.direction == expected_direction
+
+    @pytest.mark.parametrize(
+        "direction, distance, expected_position",
+        (
+                ('N', 5, 10),
+                ('S', 9, 14),
+                ('W', 1, 6),
+                ('E', 3, 8)
+        )
+    )
+    def test_move_forward(self, direction, distance, expected_position):
+        robot = Robot(self.x, self.y, self.asteroid, direction)
+        if direction == 'N' or direction == 'S':
+            current_position = robot.x
+            robot.move_forward(distance)
+        if direction == 'W' or direction == 'E':
+            current_position = robot.y
+            robot.move_forward(distance)
+        assert current_position + distance == expected_position
+
+    @pytest.mark.parametrize(
+        "direction, distance, expected_position",
+        (
+                ('N', 1, 4),
+                ('S', 3, 2),
+                ('W', 4, 1),
+                ('E', 2, 3)
+        )
+    )
+    def test_move_backward(self, direction, distance, expected_position):
+        robot = Robot(self.x, self.y, self.asteroid, direction)
+        if direction == 'N' or direction == 'S':
+            current_position = robot.x
+            robot.move_backward(distance)
+        if direction == 'W' or direction == 'E':
+            current_position = robot.y
+            robot.move_backward(distance)
+        assert current_position - distance == expected_position
+
+    @pytest.mark.parametrize(
+        "direction, distance, expected_position",
+        (
+                ('N', 15, 20),
+                ('S', 25, 30),
+                ('W', 11, 16),
+                ('E', 14, 19)
+        )
+    )
+    def test_robot_fall_forward(self, direction, distance, expected_position):
+        with pytest.raises(RobotFallError):
+            robot = Robot(self.x, self.y, self.asteroid, direction)
+            robot.move_forward(distance)
+
+    @pytest.mark.parametrize(
+        "direction, distance, expected_position",
+        (
+                ('N', 6, -1),
+                ('S', 20, -15),
+                ('W', 7, -2),
+                ('E', 10, -5)
+        )
+    )
+    def test_robot_fall_backward(self, direction, distance, expected_position):
+        with pytest.raises(RobotFallError):
+            robot = Robot(self.x, self.y, self.asteroid, direction)
+            robot.move_backward(distance)
